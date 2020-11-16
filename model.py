ee20154b40008cbe334c6f93b343fa4728824e78
Leyla Memiguven <leylamemiguven@gmail.com>
2020-11-16 10:12:04 +0300

    make it better

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
