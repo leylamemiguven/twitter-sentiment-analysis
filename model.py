ac43757e1f7acf7aca88d1998c55073bf6ac7031
Leyla Memiguven <leyla.memiguven@mail.utoronto.ca>
2024-09-09 17:05:58 -0400

    Revert "putting the keras model file in aws"
    This reverts commit a6660cbe0cba2e5f844d223f6837e5de19248579.

--- model.py
+++ model.py
@@ -1,53 +1,32 @@
 import tensorflow as tf
 from tensorflow import keras
-import boto3
-from botocore.exceptions import NoCredentialsError
+# from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import os
+import h5py
+
 
 def predict(user_input_tweet):
-    # AWS credentials and S3 bucket details
-    ACCESS_KEY = 'your-access-key'
-    SECRET_KEY = 'your-secret-key'
-    BUCKET_NAME = 'twitter-sentiment-analysis'
-    FILE_NAME = 'twitter_sentiment_analysis.h5'
 
-    def download_file_from_s3():
-        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY)
+    #load the models
+    model = tf.keras.models.load_model('/app/twitter_sentiment_analysis.h5')
 
-        try:
-            # Download file to the root directory
-            s3.download_file(BUCKET_NAME, FILE_NAME, FILE_NAME)
-            print(f"{FILE_NAME} has been downloaded.")
-        except NoCredentialsError:
-            print("Credentials not available")
-
-    download_file_from_s3()
-    
-    # Load the model
-    model_path = os.path.join(os.getcwd(), FILE_NAME)
-    model = tf.keras.models.load_model(model_path)
-
-    # Initialize the tokenizer (ensure this matches the one used for training)
+    # load tokenizer for prediction
     tk = Tokenizer(num_words=1000, filters='!"#$%&()*+,./:;<=>?@[]^_`{|}~')
 
-    # Convert user input to sequences and pad them
     tk.fit_on_texts(user_input_tweet)
     X = tk.texts_to_sequences(user_input_tweet)
     X = pad_sequences(X, maxlen=30)
-    
-    # Predict using the model
     predicted_result = model.predict(X, batch_size=1)
+    # print(predicted_result)
 
-    # Get predicted result
-    status = ['positive', 'negative']
-    if predicted_result[0, 0] > predicted_result[0, 1]:
-        status_result = status[0]
-        predicted_probability = predicted_result[0, 0]
+    #get predicted result
+    status = ['positive','negative']
+    if predicted_result.item(0)> predicted_result.item(1):
+        status = status[1]
+        predicted_probability = predicted_result.item(0)
     else:
-        status_result = status[1]
-        predicted_probability = predicted_result[0, 1]
+        status = status[0]
+        predicted_probability = predicted_result.item(1)
 
-    return status_result, predicted_probability
+    return(status, predicted_probability)
